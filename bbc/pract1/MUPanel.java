ee10488403533c14a69452884ef4d7347da9b444
cindricbb <cindricbb@users.noreply.github.com>
2024-02-14 18:04:52 -0500

    Add files via upload

---
+++ bbc/pract1/MUPanel.java
@@ -0,0 +1,66 @@
+/* CSC 120
+ * Practicum # 1
+ *
+ * Samantha Student
+ *
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class MUPanel extends JPanel {
+
+    
+	
+
+    // constructor method
+    public MUPanel() {
+        setLayout(null);
+        setPreferredSize(new Dimension(800, 600));
+        setName("Practicum # 1 authored by Samantha Student");
+        setUp();
+        setBackground(Color.WHITE);
+
+    
+	
+	
+	
+	
+	
+	
+    } // end of constructor
+    
+	
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); // This line must be first in this method!
+
+
+
+
+
+
+	
+    } // end of paintComponent()
+    
+    
+    
+       
+    
+    
+    
+    /***********************************************
+     * Do NOT change or delete anything below here!
+     ***********************************************/
+    public void setUp() {
+        for (Component c: getComponents())
+            c.setSize(c.getPreferredSize());
+        JFrame f = new JFrame(getName());
+        f.setContentPane(this);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);    
+    }
+
+    public static void main(String args[]){new MUPanel();}
+
+} // end of class MUPanel
